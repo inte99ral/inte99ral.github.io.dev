e2692ef9436acd69385837e549ee4a770415fbed
인테그랄 <108013145+inte99ral@users.noreply.github.com>
2023-01-31 23:36:40 +0900

    home page update

--- front-end/src/components/HomePage/components/HomeBanner/index.tsx
+++ front-end/src/components/HomePage/components/HomeBanner/index.tsx
@@ -1,9 +1,13 @@
+// API & Library
 import React from 'react';
+
+// Styles
+import { Styled_HomeBanner } from './style';
 
 export const HomeBanner = () => {
   return (
-    <>
-      <div></div>
-    </>
+    <Styled_HomeBanner>
+      <div>프로필이란말이다</div>
+    </Styled_HomeBanner>
   );
 };
