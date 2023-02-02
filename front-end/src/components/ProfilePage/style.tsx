7f5b42bc710c6c89f1f3d5833220e6a559e9c93d
인테그랄 <108013145+inte99ral@users.noreply.github.com>
2023-02-02 23:48:40 +0900

    profile page update 1.5v

--- front-end/src/components/ProfilePage/style.tsx
+++ front-end/src/components/ProfilePage/style.tsx
@@ -4,9 +4,10 @@ import Styled from 'styled-components';
 export const Styled_ProfilePage = Styled.div`
   height: 100vh;
   width: 100vw;
+  padding-bottom: 1rem;
 
   overflow-y: scroll;
-  
+
   display: flex;
   flex-direction: column;
   align-items: center;
