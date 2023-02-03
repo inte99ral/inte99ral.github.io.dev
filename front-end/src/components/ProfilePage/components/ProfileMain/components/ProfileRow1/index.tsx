28352ded27f5759959c6a9cc40616ff414e77399
인테그랄 <108013145+inte99ral@users.noreply.github.com>
2023-02-03 23:35:52 +0900

    profile page update 1.6v

--- front-end/src/components/ProfilePage/components/ProfileMain/components/ProfileRow1/index.tsx
+++ front-end/src/components/ProfilePage/components/ProfileMain/components/ProfileRow1/index.tsx
@@ -2,12 +2,18 @@
 import React from 'react';
 
 // Styles
-import { Styled_ProfileRow1 } from './style';
+import {
+  Styled_ProfileRow1,
+  Styled_ProfileRow1Container,
+  Styled_ProfileRow1ContainerPicture,
+} from './style';
 
 export const ProfileRow1 = () => {
   return (
-    <Styled_ProfileRow1 className="profile-row-1 outer">
-      <div>프로필1</div>
+    <Styled_ProfileRow1 className="profile-row1 outer">
+      <Styled_ProfileRow1Container>
+        <Styled_ProfileRow1ContainerPicture className="profile-row1-container-picture inner" />
+      </Styled_ProfileRow1Container>
     </Styled_ProfileRow1>
   );
 };
