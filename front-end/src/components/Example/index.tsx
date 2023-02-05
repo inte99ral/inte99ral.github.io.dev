ca6b17c637348bd44499395d6ee22962e81fb042
인테그랄 <108013145+inte99ral@users.noreply.github.com>
2023-02-06 00:41:25 +0900

    example upload

---
+++ front-end/src/components/Example/index.tsx
@@ -0,0 +1,15 @@
+// API & Library
+import React from 'react';
+
+// Styles
+import { Styled_Example } from './style';
+
+// Components
+
+export const Example = () => {
+  return (
+    <Styled_Example>
+      <div>예시</div>
+    </Styled_Example>
+  );
+};
---
+++ front-end/src/components/Sidebar/components/Example copy/style.tsx
@@ -0,0 +1,10 @@
+// API & Library
+import Styled from 'styled-components';
+
+export const Styled_Example = Styled.div`
+  position: relative;
+  height: 10px;
+  width: 10px;
+
+  background-color: blue;
+`;
