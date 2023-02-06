531c29b25be8e7bec337efcf096caf0c0cf8fff5
인테그랄 <108013145+inte99ral@users.noreply.github.com>
2023-02-06 23:29:42 +0900

    Profile Banner update 1.1v

--- front-end/src/components/ProfilePage/components/ProfileBanner/index.tsx
+++ front-end/src/components/ProfilePage/components/ProfileBanner/index.tsx
@@ -2,13 +2,23 @@
 import React from 'react';
 
 // Styles
-import { Styled_ProfileBanner, Styled_ProfileBannerPhoto } from './style';
+import {
+  Styled_ProfileBanner,
+  Styled_ProfileBannerPhoto,
+  Styled_ProfileBannerTitle,
+  Styled_ProfileBannerSubtitle,
+} from './style';
 
 export const ProfileBanner = () => {
   return (
     <Styled_ProfileBanner className="profile-banner outer">
-      <Styled_ProfileBannerPhoto></Styled_ProfileBannerPhoto>
-      <div>프로필이란말이다</div>
+      <Styled_ProfileBannerPhoto className="profile-banner-photo inner" />
+      <Styled_ProfileBannerTitle className="profile-banner-title">
+        PARK JUN HYUK
+      </Styled_ProfileBannerTitle>
+      <Styled_ProfileBannerSubtitle className="profile-banner-subtitle">
+        UIᆞUXᆞFront-End Developer
+      </Styled_ProfileBannerSubtitle>
     </Styled_ProfileBanner>
   );
 };
--- front-end/src/components/ProfilePage/components/ProfileBanner/style.tsx
+++ front-end/src/components/ProfilePage/components/ProfileBanner/style.tsx
@@ -2,7 +2,9 @@
 import Styled from 'styled-components';
 
 // Assets
-import Photo from './assets/images/Photo.png';
+// import Photo from './assets/images/Photo.png';
+// import Photo from './assets/images/Personal.png';
+import Photo from './assets/images/PersonalDJ.png';
 
 export const Styled_ProfileBanner = Styled.div`
   width: 100%;
@@ -12,17 +14,35 @@ export const Styled_ProfileBanner = Styled.div`
   flex-shrink: 0;
 
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 
-  background: linear-gradient( -45deg, #506EC8, #E1C8FA, #F078C8);
+  /* background: linear-gradient( -45deg, #506EC8, #E1C8FA, #F078C8); */
+  background-color: #44546A;
 `;
 
 export const Styled_ProfileBannerPhoto = Styled.div`
-  width: 10px;
-  height: 10px;
+  width: 12rem;
+  height: 12rem;
+  margin: 1rem;
+
+  border-radius: 50%;
+  border: 0.5rem solid #e4e5e7;
 
   background-image: url(${Photo});
   background-repeat: no-repeat;
   background-size: cover;
+`;
+
+export const Styled_ProfileBannerTitle = Styled.div`
+  font-family: CopperplateGothicBoldRegular;
+  font-size: 1.5rem;
+  color: #e4e5e7;
+`;
+
+export const Styled_ProfileBannerSubtitle = Styled.div`
+  font-family: CopperplateGothicBoldRegular;
+  font-size: 1rem;
+  color: #333F50;
 `;
