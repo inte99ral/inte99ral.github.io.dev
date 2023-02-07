6a3cdcd749adddba6a6c9fd27ad1a7ba504eec87
인테그랄 <108013145+inte99ral@users.noreply.github.com>
2023-02-07 22:37:30 +0900

    Profile Banner update 1.2v

---
+++ front-end/src/components/ProfilePage/components/ProfileMain/components/ProfileRow1/components/ProfileRow1Header/index.tsx
@@ -0,0 +1,9 @@
+// API & Library
+import React from 'react';
+
+// Styles
+import { Styled_ProfileRow1Header } from './style';
+
+export const ProfileRow1Header = () => {
+  return <Styled_ProfileRow1Header className="profile-row1-header">ad</Styled_ProfileRow1Header>;
+};
---
+++ front-end/src/components/ProfilePage/components/ProfileMain/components/ProfileRow1/components/ProfileRow1Header/style.tsx
@@ -0,0 +1,7 @@
+// API & Library
+import Styled from 'styled-components';
+
+export const Styled_ProfileRow1Header = Styled.div`
+  height: 10px;
+  width: 10px;
+`;
