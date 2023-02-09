ef58de53f53940b7650ac454e0ffc85c8705630b
인테그랄 <108013145+inte99ral@users.noreply.github.com>
2023-02-10 00:42:38 +0900

    Profile Banner update 1.4v

--- front-end/src/components/ProfilePage/components/ProfileBanner/index.tsx
+++ front-end/src/components/ProfilePage/components/ProfileBanner/index.tsx
@@ -1,5 +1,5 @@
 // API & Library
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 // Styles
 import {
@@ -10,9 +10,27 @@ import {
 } from './style';
 
 export const ProfileBanner = () => {
+  // Init
+  const ProfileBannerPhoto = useRef<HTMLDivElement>(null);
+
+  // Methods
+  const handleScroll = () => {
+    // console.log('scrolled');
+    console.log(ProfileBannerPhoto.current?.scrollTop);
+  };
+
+  // LifeCycle
+  useEffect(() => {
+    console.log('걍때워넣음');
+    // window.addEventListener('keypress', handleScroll);
+    // return () => {
+    //   window.removeEventListener('scroll', handleScroll);
+    // };
+  }, []);
+
   return (
     <Styled_ProfileBanner className="profile-banner outer">
-      <Styled_ProfileBannerPhoto className="profile-banner-photo inner" />
+      <Styled_ProfileBannerPhoto className="profile-banner-photo inner" ref={ProfileBannerPhoto} />
       <Styled_ProfileBannerTitle className="profile-banner-title">
         PARK JUN HYUK
       </Styled_ProfileBannerTitle>
