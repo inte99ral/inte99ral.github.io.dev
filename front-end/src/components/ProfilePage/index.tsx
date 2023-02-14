565eddb744a35888a5879aad954a2eac65ad7ec8
인테그랄 <108013145+inte99ral@users.noreply.github.com>
2023-02-14 23:37:46 +0900

    recoil setting

--- front-end/src/components/ProfilePage/index.tsx
+++ front-end/src/components/ProfilePage/index.tsx
@@ -1,5 +1,7 @@
 // API & Library
 import React, { useRef, useEffect } from 'react';
+import { useRecoilState } from 'recoil';
+import { count } from 'store';
 
 // Styles
 import { Styled_ProfilePage } from './style';
@@ -11,11 +13,17 @@ import { ProfileMain } from './components/ProfileMain';
 export const ProfilePage = () => {
   // Init
   const ProfilePage = useRef<HTMLDivElement>(null);
+  const [counter, setCounter] = useRecoilState(count);
 
   // Methods
   const handleScroll = () => {
     // console.log('scrolled');
-    console.log(ProfilePage.current?.scrollTop);
+    console.log(ProfilePage.current?.scroll({ top: 0, behavior: 'smooth' }));
+  };
+
+  const countUp = () => {
+    // console.log('scrolled');
+    setCounter((x) => x + 1);
   };
 
   // LifeCycle
@@ -27,8 +35,17 @@ export const ProfilePage = () => {
     // };
   }, []);
 
+  useEffect(() => {
+    console.log('걍때워넣1음');
+  }, [ProfilePage.current?.scrollTop]);
+
   return (
     <Styled_ProfilePage className="profile-page" ref={ProfilePage}>
+      <div>테스트용: {counter}</div>
+      <div>테스트용: {ProfilePage.current?.scrollTop}</div>
+      <button onClick={countUp} style={{ height: 100, width: 100 }}>
+        카운터
+      </button>
       <ProfileBanner />
       <ProfileMain />
     </Styled_ProfilePage>
--- front-end/src/store.tsx
+++ front-end/src/store.tsx
@@ -1,13 +1,13 @@
-import { atom, useRecoilState } from 'recoil';
+import { atom } from 'recoil';
 
 // State
 export const count = atom({
   key: 'count',
-  default: 2,
+  default: 6,
 });
 
 // Action
-export const countUp = atom({
-  key: 'count',
+export const data = atom({
+  key: 'data',
   default: 2,
 });
