172d7a1cffac9d4ba26cdee87201a77f9c0761c7
인테그랄 <108013145+inte99ral@users.noreply.github.com>
2023-08-12 21:36:59 +0900

    Update Homepage Banner

--- front-end/src/components/HomePage/components/HomeBanner/style.tsx
+++ front-end/src/components/HomePage/components/HomeBanner/style.tsx
@@ -1,6 +1,10 @@
 // API & Library
 import Styled from 'styled-components';
 
+// Asset
+import BG from './asset/image/LW001.jpg';
+
+// Export
 export const Styled_HomeBanner = Styled.div`
   min-height: 20rem;
   width: 100%;
@@ -12,4 +16,16 @@ export const Styled_HomeBanner = Styled.div`
   /* background: linear-gradient(-45deg, #f078c8, #e1c8fa, #506ec8); */
   background-color: var(--color-main);
   z-index: 2500;
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    background-image: url(${BG});
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
 `;
