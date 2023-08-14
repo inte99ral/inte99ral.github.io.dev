e1bc82285e67b6adeac6ce7cfb31dc0d55dee48b
인테그랄 <108013145+inte99ral@users.noreply.github.com>
2023-08-14 23:51:25 +0900

    Update HomeBanner

--- front-end/src/components/HomePage/components/HomeBanner/index.tsx
+++ front-end/src/components/HomePage/components/HomeBanner/index.tsx
@@ -2,11 +2,12 @@
 import React from 'react';
 
 // Style
-import { Styled_HomeBanner } from './style';
+import { Styled_HomeBanner, Styled_HomeBannerBG } from './style';
 
 export const HomeBanner = () => {
   return (
     <Styled_HomeBanner className="home-banner outer">
+      <Styled_HomeBannerBG />
       <div>프로필이란말이다</div>
     </Styled_HomeBanner>
   );
--- front-end/src/components/HomePage/components/HomeBanner/style.tsx
+++ front-end/src/components/HomePage/components/HomeBanner/style.tsx
@@ -6,26 +6,29 @@ import BG from './asset/image/LW001.jpg';
 
 // Export
 export const Styled_HomeBanner = Styled.div`
-  min-height: 20rem;
+  min-height: 30rem;
   width: 100%;
 
   display: flex;
   justify-content: center;
   align-items: center;
 
-  /* background: linear-gradient(-45deg, #f078c8, #e1c8fa, #506ec8); */
+  background: linear-gradient(-45deg, #f078c8, #e1c8fa, #506ec8);
   background-color: var(--color-main);
   z-index: 2500;
+`;
 
-  &::after {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 100%;
-    width: 100%;
-    background-image: url(${BG});
-    background-repeat: no-repeat;
-    background-size: cover;
-  }
+export const Styled_HomeBannerBG = Styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  background-image: url(${BG});
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  mix-blend-mode: screen;
+  filter: contrast(1.2);
+  opacity: 0.5;
 `;
